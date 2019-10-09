c990aa8abdf186083b3807ed94ecb969b06387f3
Oliver Barbeau <oliver.barbeau@gmail.com>
2019-10-08 20:21:22 -0500

    Delete twoSum.py

--- twoSum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        comps = {}
-        for i in range(len(nums)):
-            if nums[i] in comps:
-                return [comps[nums[i]], i]
-            else:
-                comps[target-nums[i]] = i
-        return [0,0]
