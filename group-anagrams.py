d4ad933db0f973f9a52a0ac94b9bc5f7e9a881a7
Oliver Barbeau <oliver.barbeau@gmail.com>
2019-10-09 19:58:38 -0500

    Add problem descriptions

--- group-anagrams.py
+++ group-anagrams.py
@@ -1,5 +1,21 @@
+# Given an array of strings, group anagrams together.
+
+# Example:
+
+# Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+# Output:
+# [
+#   ["ate","eat","tea"],
+#   ["nat","tan"],
+#   ["bat"]
+# ]
+# Note:
+
+# All inputs will be in lowercase.
+# The order of your output does not matter.
+
 import string
-#start: 1:36PM 10/8/2019
+
 class Solution:
     letterToPrime = {}
     primes = "2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103"
